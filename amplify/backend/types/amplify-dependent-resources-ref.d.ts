c9e935ae95bea34329354d5df11f23908f1e910f
yusufferdogan <yusuffx03@gmail.com>
2024-08-25 18:49:36 +0300

    add amplify to  ignore

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "amplifysocialfilayer1ef80ee8": {
+      "Arn": "string"
+    }
+  },
+  "storage": {
+    "socialfidb": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
